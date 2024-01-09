4777631d2804e951f8cd069b3ad1f57701479e00
manjiri-chandure <chanduremanjiri@gmail.com>
2024-01-09 16:38:35 +0530

    initial commit

---
+++ src/main/java/com/example/payroll/PayrollApplication.java
@@ -0,0 +1,18 @@
+package com.example.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class PayrollApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PayrollApplication.class, args);
+
+	}
+
+}
